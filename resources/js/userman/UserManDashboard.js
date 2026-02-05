193d8b97e7e0a1db467f3343431160d5426366cc
Kenjieeee <delacruzkenjie1206@gmail.com>
2026-02-05 08:20:42 +0800

    MINIMAL CHANGES IN USERS TAB AND RESPONSIVENESS

--- resources/js/userman/UserManDashboard.js
+++ resources/js/userman/UserManDashboard.js
@@ -25,18 +25,21 @@ const dashboardModal = document.getElementById("dashboard_department_modal");
 const dashboardOpen = document.getElementById("dashboard_department_box");
 const dashboardClose = document.getElementById("dashboard_close_modal");
 
-dashboardOpen.addEventListener("click", () => {
-    dashboardModal.style.display = "flex";
-});
+if (dashboardModal && dashboardOpen && dashboardClose) {
+    dashboardOpen.addEventListener("click", () => {
+        dashboardModal.style.display = "flex";
+    });
 
-dashboardClose.addEventListener("click", () => {
-    dashboardModal.style.display = "none";
-});
-
-window.addEventListener("click", (e) => {
-    if (e.target === dashboardModal) {
+    dashboardClose.addEventListener("click", () => {
         dashboardModal.style.display = "none";
-    }
-});
+    });
+
+    window.addEventListener("click", (e) => {
+        if (e.target === dashboardModal) {
+            dashboardModal.style.display = "none";
+        }
+    });
+}
+
 
 
